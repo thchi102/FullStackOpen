3e4dffdf5bbf919fd6a805504ae7ba5d1be85659
Jonathan Chi <thchi102@gmail.com>
2025-05-02 14:18:13 +0800

    configure for vercel

--- part3/phonebook_backend/api/index.js
+++ part3/phonebook_backend/api/index.js
@@ -121,5 +121,6 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)})
 console.log(`Server running on port ${PORT}`)
